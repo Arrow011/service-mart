c38d6a60582a48f78dba0a23891f1f4a5ba0a000
Ankur <ankurveer0522@gmail.com>
2023-03-22 18:29:39 +0530

    Added micro-services for service-mart application

---
+++ product-service/src/main/java/com/project/productservice/controller/ProductController.java
@@ -0,0 +1,42 @@
+package com.project.productservice.controller;
+import com.project.productservice.model.ProductRequest;
+import com.project.productservice.model.ProductResponse;
+import com.project.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping("/add")
+    public ResponseEntity<Long> addProducts(@RequestBody ProductRequest productRequest){
+        long product_id = productService.addProduct(productRequest);
+        return new ResponseEntity<>(product_id,HttpStatus.CREATED);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<ProductResponse>> getAllProducts(){
+        List<ProductResponse> listOfProductResponse = productService.fetchAllProducts();
+        return new ResponseEntity<>(listOfProductResponse,HttpStatus.OK);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable("id") Long productId){
+        ProductResponse productResponse = productService.getProductById(productId);
+        return new ResponseEntity<>(productResponse,HttpStatus.OK);
+    }
+
+    @PutMapping("/updateQuantity/{id}")
+    public ResponseEntity<Void> updateProductQuantity(@PathVariable("id") long productId, @RequestParam("quantity") long quantity){
+        productService.updateQuantity(productId,quantity);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
