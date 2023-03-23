215133c6fd4768e033349853f9dada6d4835893c
Ankur <ankurveer0522@gmail.com>
2023-03-23 13:52:19 +0530

    Added security in order-service using okta oauth2 and added interceptors to add the jwt access token before making request to product and payment service.

--- order-service/src/main/java/com/project/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/project/orderservice/controller/OrderController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,14 +16,14 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
-
+    @PreAuthorize("hasAuthority('Customer')")
     @PostMapping("/placeOrder")
     public ResponseEntity<String> placeOrder(@RequestBody OrderRequest orderRequest){
         long orderId = orderService.placeOrder(orderRequest);
         log.info("Order place with Id: {}",orderId);
         return new ResponseEntity<>("Order Successfully placed with Id: "+orderId, HttpStatus.CREATED);
     }
-
+    @PreAuthorize("hasAuthority('Admin') || hasAuthority('Customer')")
     @GetMapping("/{orderId}")
     public ResponseEntity<OrderResponse> getOrderDetails(@PathVariable("orderId") long orderId){
         OrderResponse orderResponse = orderService.getOrderDetails(orderId);
